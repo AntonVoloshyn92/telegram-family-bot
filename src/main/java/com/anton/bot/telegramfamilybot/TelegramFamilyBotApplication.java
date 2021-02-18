e1a09b74268a805fed2fa255fddd2436abba4aa4
AntonVoloshyn92 <sverin22@gmail.com>
2021-02-18 13:00:46 +0200

    first commit

---
+++ src/main/java/com/anton/bot/telegramfamilybot/TelegramFamilyBotApplication.java
@@ -0,0 +1,13 @@
+package com.anton.bot.telegramfamilybot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramFamilyBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramFamilyBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anton/bot/telegramfamilybot/TelegramFamilyBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anton.bot.telegramfamilybot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramFamilyBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
